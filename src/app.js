708c911797b5500b5c8c78568c297690c89838e3
JuanS072 <juancarlossequeira07@gmail.com>
2023-09-19 16:15:51 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const routers = require('./routes/index.routes'); 
+const cors = require("cors")
 
+app.use(cors());
 app.use(express.json())
 app.use('/', routers);
 
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
 
 app.listen(3000, ()=>{
     console.log('Servidor a la espera de conexiones');   
